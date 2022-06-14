5661b17ef195ab84ba15a454e97e33723d68efa4
Noor Al Deen Dwaikat <noordwaikat6@gmail.com>
2022-06-14 13:45:21 +0300

    Update Stock

--- src/Stock/stock.controller.ts
+++ src/Stock/stock.controller.ts
@@ -1,35 +1,46 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
-import { StockEntity } from "./stock.entity";
-import { StockService } from "./stock.service";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { StockEntity } from './stock.entity';
+import { StockService } from './stock.service';
 
 @Controller('api/stocks')
 export class StockController {
+  constructor(private _stockService: StockService) {}
 
-    constructor(private _stockService: StockService) {}
+  // Add new stock
+  @Post()
+  addStock(@Body() stock: StockEntity) {
+    return this._stockService.addStock(stock);
+  }
 
-    // Add new stock
-    @Post()
-    addStock(@Body() stock: StockEntity) {
-        return this._stockService.addStock(stock);
-    }
+  // Delete stock
+  @Delete(':id')
+  deleteStock(@Param('id') id: number) {
+    return this._stockService.deleteStock(id);
+  }
 
-    // Delete stock
-    @Delete(':id')
-    deleteStock(@Param('id') id: number) {
-        return this._stockService.deleteStock(id)
-    }
+  // Get All Stocks
+  @Get()
+  getAllStocks() {
+    return this._stockService.getAllStocks();
+  }
 
-    // Get All Stocks
-    @Get()
-    getAllStocks() {
-        return this._stockService.getAllStocks();
-    }
+  // Get Specific Stock
+  @Get(':id')
+  getStock(@Param('id') id: number) {
+    return this._stockService.getStock(id);
+  }
 
-    // Get Specific Stock
-    @Get(':id')
-    getStock(@Param('id') id: number) {
-        return this._stockService.getStock(id);
-    }
-
+  // Update Stock
+  @Put(':id')
+  updateStock(@Param('id') id: number, @Body() stock: StockEntity) {
+    return this._stockService.updateStock(id, stock);
+  }
 }
--- src/Stock/stock.service.ts
+++ src/Stock/stock.service.ts
@@ -1,31 +1,42 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { StockEntity } from "./stock.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { StockEntity } from './stock.entity';
 
 @Injectable()
 export class StockService {
-    constructor(@InjectRepository(StockEntity) private _stockRepo: Repository<StockEntity>) {}
+  constructor(
+    @InjectRepository(StockEntity) private _stockRepo: Repository<StockEntity>,
+  ) {}
 
-    // Add new stock
-    async addStock(stock: StockEntity) {
-        let createdEntity = this._stockRepo.create(stock);
-        await this._stockRepo.save(createdEntity);
-        return createdEntity;
-    }
+  // Add new stock
+  async addStock(stock: StockEntity) {
+    let createdEntity = this._stockRepo.create(stock);
+    await this._stockRepo.save(createdEntity);
+    return createdEntity;
+  }
 
-    // Delete stock
-    async deleteStock(id: number) {
-        return await this._stockRepo.delete(id)
-    }
+  // Delete stock
+  async deleteStock(id: number) {
+    return await this._stockRepo.delete(id);
+  }
 
-    // Get All Stocks
-    async getAllStocks() {
-        return await this._stockRepo.find();
-    }
+  // Get All Stocks
+  async getAllStocks() {
+    return await this._stockRepo.find();
+  }
 
-    // Get Specific Stock
-    async getStock(id: number) {
-        return await this._stockRepo.findOneBy({SN: id})
+  // Get Specific Stock
+  async getStock(id: number) {
+    return await this._stockRepo.findOneBy({ SN: id });
+  }
+
+  // Update Stock
+  async updateStock(id: number, stock: StockEntity) {
+    let stk = await this._stockRepo.findOneBy({ SN: id });
+    if (stk) {
+      return await this._stockRepo.update({ SN: id }, stock);
     }
+    return 'Stock Not Found';
+  }
 }
